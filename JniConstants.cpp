jonnyandrew/libnativehelper

Track the move of various libcore.* classes to android.*.
Change-Id: Ib5852584f011fec69790a4a019b252a071c05871

JniConstants.cpp
@@ -98,11 +98,11 @@ void JniConstants::init(JNIEnv* env) {
     floatClass = findClass(env, "java/lang/Float");
     deflaterClass = findClass(env, "java/util/zip/Deflater");
     doubleClass = findClass(env, "java/lang/Double");
-    errnoExceptionClass = findClass(env, "libcore/io/ErrnoException");
+    errnoExceptionClass = findClass(env, "android/os/ErrnoException");
     fieldClass = findClass(env, "java/lang/reflect/Field");
     fieldPositionIteratorClass = findClass(env, "libcore/icu/NativeDecimalFormat$FieldPositionIterator");
     fileDescriptorClass = findClass(env, "java/io/FileDescriptor");
-    gaiExceptionClass = findClass(env, "libcore/io/GaiException");
+    gaiExceptionClass = findClass(env, "android/os/GaiException");
     inet6AddressClass = findClass(env, "java/net/Inet6Address");
     inetAddressClass = findClass(env, "java/net/InetAddress");
     inetSocketAddressClass = findClass(env, "java/net/InetSocketAddress");
@@ -113,8 +113,8 @@ void JniConstants::init(JNIEnv* env) {
     localeDataClass = findClass(env, "libcore/icu/LocaleData");
     longClass = findClass(env, "java/lang/Long");
     methodClass = findClass(env, "java/lang/reflect/Method");
-    mutableIntClass = findClass(env, "libcore/util/MutableInt");
-    mutableLongClass = findClass(env, "libcore/util/MutableLong");
+    mutableIntClass = findClass(env, "android/util/MutableInt");
+    mutableLongClass = findClass(env, "android/util/MutableLong");
     objectClass = findClass(env, "java/lang/Object");
     objectArrayClass = findClass(env, "[Ljava/lang/Object;");
     outputStreamClass = findClass(env, "java/io/OutputStream");
@@ -126,16 +126,16 @@ void JniConstants::init(JNIEnv* env) {
     socketClass = findClass(env, "java/net/Socket");
     socketImplClass = findClass(env, "java/net/SocketImpl");
     stringClass = findClass(env, "java/lang/String");
-    structAddrinfoClass = findClass(env, "libcore/io/StructAddrinfo");
-    structFlockClass = findClass(env, "libcore/io/StructFlock");
-    structGroupReqClass = findClass(env, "libcore/io/StructGroupReq");
-    structGroupSourceReqClass = findClass(env, "libcore/io/StructGroupSourceReq");
-    structLingerClass = findClass(env, "libcore/io/StructLinger");
-    structPasswdClass = findClass(env, "libcore/io/StructPasswd");
-    structPollfdClass = findClass(env, "libcore/io/StructPollfd");
-    structStatClass = findClass(env, "libcore/io/StructStat");
-    structStatVfsClass = findClass(env, "libcore/io/StructStatVfs");
-    structTimevalClass = findClass(env, "libcore/io/StructTimeval");
-    structUcredClass = findClass(env, "libcore/io/StructUcred");
-    structUtsnameClass = findClass(env, "libcore/io/StructUtsname");
+    structAddrinfoClass = findClass(env, "android/os/StructAddrinfo");
+    structFlockClass = findClass(env, "android/os/StructFlock");
+    structGroupReqClass = findClass(env, "android/os/StructGroupReq");
+    structGroupSourceReqClass = findClass(env, "android/os/StructGroupSourceReq");
+    structLingerClass = findClass(env, "android/os/StructLinger");
+    structPasswdClass = findClass(env, "android/os/StructPasswd");
+    structPollfdClass = findClass(env, "android/os/StructPollfd");
+    structStatClass = findClass(env, "android/os/StructStat");
+    structStatVfsClass = findClass(env, "android/os/StructStatVfs");
+    structTimevalClass = findClass(env, "android/os/StructTimeval");
+    structUcredClass = findClass(env, "android/os/StructUcred");
+    structUtsnameClass = findClass(env, "android/os/StructUtsname");
 }

vakkasa/android_libnativehelper

JniConstants.cpp
@@ -98,11 +98,11 @@ void JniConstants::init(JNIEnv* env) {
     floatClass = findClass(env, "java/lang/Float");
     deflaterClass = findClass(env, "java/util/zip/Deflater");
     doubleClass = findClass(env, "java/lang/Double");
-    errnoExceptionClass = findClass(env, "libcore/io/ErrnoException");
+    errnoExceptionClass = findClass(env, "android/system/ErrnoException");
     fieldClass = findClass(env, "java/lang/reflect/Field");
     fieldPositionIteratorClass = findClass(env, "libcore/icu/NativeDecimalFormat$FieldPositionIterator");
     fileDescriptorClass = findClass(env, "java/io/FileDescriptor");
-    gaiExceptionClass = findClass(env, "libcore/io/GaiException");
+    gaiExceptionClass = findClass(env, "android/system/GaiException");
     inet6AddressClass = findClass(env, "java/net/Inet6Address");
     inetAddressClass = findClass(env, "java/net/InetAddress");
     inetSocketAddressClass = findClass(env, "java/net/InetSocketAddress");
@@ -113,8 +113,8 @@ void JniConstants::init(JNIEnv* env) {
     localeDataClass = findClass(env, "libcore/icu/LocaleData");
     longClass = findClass(env, "java/lang/Long");
     methodClass = findClass(env, "java/lang/reflect/Method");
-    mutableIntClass = findClass(env, "libcore/util/MutableInt");
-    mutableLongClass = findClass(env, "libcore/util/MutableLong");
+    mutableIntClass = findClass(env, "android/util/MutableInt");
+    mutableLongClass = findClass(env, "android/util/MutableLong");
     objectClass = findClass(env, "java/lang/Object");
     objectArrayClass = findClass(env, "[Ljava/lang/Object;");
     outputStreamClass = findClass(env, "java/io/OutputStream");
@@ -126,16 +126,16 @@ void JniConstants::init(JNIEnv* env) {
     socketClass = findClass(env, "java/net/Socket");
     socketImplClass = findClass(env, "java/net/SocketImpl");
     stringClass = findClass(env, "java/lang/String");
-    structAddrinfoClass = findClass(env, "libcore/io/StructAddrinfo");
-    structFlockClass = findClass(env, "libcore/io/StructFlock");
-    structGroupReqClass = findClass(env, "libcore/io/StructGroupReq");
-    structGroupSourceReqClass = findClass(env, "libcore/io/StructGroupSourceReq");
-    structLingerClass = findClass(env, "libcore/io/StructLinger");
-    structPasswdClass = findClass(env, "libcore/io/StructPasswd");
-    structPollfdClass = findClass(env, "libcore/io/StructPollfd");
-    structStatClass = findClass(env, "libcore/io/StructStat");
-    structStatVfsClass = findClass(env, "libcore/io/StructStatVfs");
-    structTimevalClass = findClass(env, "libcore/io/StructTimeval");
-    structUcredClass = findClass(env, "libcore/io/StructUcred");
-    structUtsnameClass = findClass(env, "libcore/io/StructUtsname");
+    structAddrinfoClass = findClass(env, "android/system/StructAddrinfo");
+    structFlockClass = findClass(env, "android/system/StructFlock");
+    structGroupReqClass = findClass(env, "android/system/StructGroupReq");
+    structGroupSourceReqClass = findClass(env, "android/system/StructGroupSourceReq");
+    structLingerClass = findClass(env, "android/system/StructLinger");
+    structPasswdClass = findClass(env, "android/system/StructPasswd");
+    structPollfdClass = findClass(env, "android/system/StructPollfd");
+    structStatClass = findClass(env, "android/system/StructStat");
+    structStatVfsClass = findClass(env, "android/system/StructStatVfs");
+    structTimevalClass = findClass(env, "android/system/StructTimeval");
+    structUcredClass = findClass(env, "android/system/StructUcred");
+    structUtsnameClass = findClass(env, "android/system/StructUtsname");
 }